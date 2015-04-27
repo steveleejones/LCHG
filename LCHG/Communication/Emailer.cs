81fd883a9b5f98a76f8be44c7e621fa7b01626d6
SteveJones <steveleejones@gmail.com>
2015-04-27 13:01:31 +0100

    initial commit

---
+++ LCHG/Communication/Emailer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCHG.Communication
+{
+    // general emailer class.
+    public class Emailer
+    {
+        private const string USER_NAME = "LOWCOSTBEDS\\SQLAdmin";
+        private const string PASSWORD = "SQL4dmin123";
+        private const string HOST_NAME = "svrexch1.lowcostbeds.com";
+        private const string FROM_NAME = "PPCEngine@lowcostholidays.com";
+
+        private const int PORT = 587;
+
+        public static void SendEmail(string toEmail, string subject, string message)
+        {
+            MailMessage mail = new MailMessage(FROM_NAME, toEmail, subject, message);
+            mail.SubjectEncoding = Encoding.UTF8;
+            mail.Priority = MailPriority.Normal;
+
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential(USER_NAME, PASSWORD);
+            client.Port = PORT;
+            client.Host = HOST_NAME;
+            client.EnableSsl = false;
+            client.UseDefaultCredentials = true;
+
+            try
+            {
+                client.Send(mail);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static void SendEmail(string toEmail, string subject, string function, Exception exception)
+        {
+            string error = "Error occured in : " + function + " : " + Environment.NewLine + Environment.NewLine;
+            error += CreateExceptionMessage(exception);
+            SendEmail(toEmail, subject, error);
+        }
+
+        private static void SendEmail(string toEmail, string subject, Exception exception)
+        {
+            SendEmail(toEmail, subject, CreateExceptionMessage(exception));
+        }
+
+        private static string CreateExceptionMessage(Exception exception)
+        {
+            string exceptionMessage;
+
+            exceptionMessage = exception.Message + Environment.NewLine + Environment.NewLine;
+            exceptionMessage += exception.StackTrace + Environment.NewLine + Environment.NewLine;
+
+            if (exception.InnerException != null)
+            {
+                exceptionMessage = "InnerException" + Environment.NewLine + Environment.NewLine;
+                exceptionMessage += exception.InnerException.Message + Environment.NewLine + Environment.NewLine;
+                exceptionMessage += exception.InnerException.StackTrace + Environment.NewLine + Environment.NewLine;
+            }
+            return exceptionMessage;
+        }
+    }
+}
---
+++ LCHG/Utilities/PackageCodeGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LCHG.Utilities
+{
+    public class PackageCodeGenerator
+    {
+        private const string CHAR_LIST = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+        public static string GeneratePackageCode(int propertyReferenceId, int duration, DateTime deptDate, int mealBasisId, int departureAirportId, int adults, int children, int totalPrice)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(Encode(propertyReferenceId, 4));
+            sb.Append(Encode(duration, 1));
+            sb.Append(Encode((((deptDate.Year % 3 * 12) - 1) + deptDate.Month), 1));
+            sb.Append(Encode(deptDate.Day, 1));
+            sb.Append(Encode(mealBasisId, 1));
+            sb.Append(Encode(departureAirportId, 2));
+            sb.Append(Encode(adults * 5 + children, 1));
+            sb.Append(Encode((totalPrice), 4)); //Removing the times 100 as will do that before here
+
+            return sb.ToString();
+        }
+
+        private static String Encode(int input, int maxLength)
+        {
+
+            string valueItem = Base36.NumberToBase36(input);
+
+            if (valueItem.Length != maxLength)
+            {
+                if (valueItem.Length < maxLength)
+                    valueItem = valueItem.PadLeft(maxLength, '0');
+                else
+                    valueItem = valueItem.Remove(maxLength);
+            }
+
+            return valueItem;
+        }
+
+        private static Int64 Decode(string input)
+        {
+            var reversed = input.ToLower().Reverse();
+            long result = 0;
+            int pos = 0;
+            foreach (char c in reversed)
+            {
+                result += CHAR_LIST.IndexOf(c) * (long)Math.Pow(36, pos);
+                pos++;
+            }
+            return result;
+        }
+    }
+}
